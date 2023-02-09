edd05e3061d145e140ad110551d1cdc74c1d23aa
SpectreKilo <johnho8788@gmail.com>
2023-02-08 21:55:10 -0500

    added code to utils

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,6 @@ const blogRoutes = require('./blogRoutes');
 const userRoutes = require('./userRoutes');
 const commentRoute = require('./commentRoute')
 
-//these will navigate to the url on /page and go to the directory that is the constant - ('/page', const of page route)
 router.use('/blog_post', blogRoutes);
 router.use('/users', userRoutes)
 router.use('/commentRoute', commentRoute)
